473de7f91d886944dc4be2418b2a086c696e6617
fruit13ok <fruit13ok@gmail.com>
2018-10-16 14:55:13 -0700

    heroku

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/AspergerNetwork",  { useMongoClient: true });
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/AspergerNetwork");
 
 module.exports.User = require('./user.js');
 module.exports.Type = require('./type.js');
